3d848f748da754885579f66dce16090d9b190b7f
jchan3990 <jonchan3990@gmail.com>
2020-07-20 12:23:38 -0700

    Finished k6 with 1900rps

--- server/index.js
+++ server/index.js
@@ -1,68 +1,77 @@
+require('newrelic')
 const path = require('path');
+const cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const db = require('../database/index.js')
-const app = express();
 
-const port = 3000;
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+      // Create a worker
+      cluster.fork();
+  }
+} else {
+  const port = 3000;
+  const app = express();
 
-// Fix cross origin
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('./public'));
-
-app.get('/api/homes/:id', (req, res) => {
-  const { id } = req.params;
-  const { homeZip } = req.body;
-  let queryStr = `SELECT home_price FROM listings WHERE listing_id=${id}`;
-  db.query(queryStr, (err, results) => {
-    if (err) {
-      console.log(err.stack);
-    } else {
-      res.status(200).send(results.rows);
-    }
+  // Fix cross origin
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
   });
-})
 
-app.post('/api/homes', (req, res) => {
-  var currentRowCount = 0;
-  db.query(`SELECT MAX(listing_id) from listings`, (err, result) => {
-    if (err) {
-      console.log(err.stack);
-    } else {
-      nextRowCount = result.rows[0].max + 1;
-      const { homePrice, homeAddress, homeZip } = req.body;
-      let queryStr = `INSERT INTO listings(listing_id, home_price, home_address, home_zip) VALUES (${nextRowCount}, ${homePrice}, '${homeAddress}', '${homeZip}')`;
-      db.query(queryStr, (err, results) => {
-        if (err) {
-          console.log(err.stack);
-        } else {
-          res.status(201).send(results.rows);
-        }
-      })
-    }
-  });
-})
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static('./public'));
 
-app.patch('/api/homes/:id', (req, res) => {
-  const { id } = req.params;
-  const { newPrice } = req.body;
-  let queryStr = `UPDATE listings SET home_price=${newPrice} WHERE listing_id=${id}`;
-  db.query(queryStr, (err, results) =>  {
-    if (err) {
-      console.log(err.stack);
-    } else {
-      res.status(201).send(results.rows);
-    }
-  });
-})
+  app.get('/api/homes/:id', (req, res) => {
+    const { id } = req.params;
+    const { homeZip } = req.body;
+    let queryStr = `SELECT home_price FROM listings WHERE listing_id=${id}`;
+    db.query(queryStr, (err, results) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        res.status(200).send(results.rows);
+      }
+    });
+  })
+
+  app.post('/api/homes', (req, res) => {
+    var currentRowCount = 0;
+    db.query(`SELECT MAX(listing_id) from listings`, (err, result) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        nextRowCount = result.rows[0].max + 1;
+        const { homePrice, homeAddress, homeZip } = req.body;
+        let queryStr = `INSERT INTO listings(listing_id, home_price, home_address, home_zip) VALUES (${nextRowCount}, ${homePrice}, '${homeAddress}', '${homeZip}')`;
+        db.query(queryStr, (err, results) => {
+          if (err) {
+            console.log(err.stack);
+          } else {
+            res.status(201).send(results.rows);
+          }
+        })
+      }
+    });
+  })
+
+  app.patch('/api/homes/:id', (req, res) => {
+    const { id } = req.params;
+    const { newPrice } = req.body;
+    let queryStr = `UPDATE listings SET home_price=${newPrice} WHERE listing_id=${id}`;
+    db.query(queryStr, (err, results) =>  {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        res.status(201).send(results.rows);
+      }
+    });
+  })
 
 
-app.listen(port, () => console.log(`\nlistening at http://localhost:${port}`));
-
+  app.listen(port, () => console.log(`\nlistening at http://localhost:${port}`));
+}
--- server/loadTest.js
+++ server/loadTest.js
@@ -2,14 +2,19 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
+  // vus: 1500,
+  // duration: '30s'
   stages: [
-    { duration: '1s', target: 1000 },
-    { duration: '1m', target: 2000 },
-  ],
+    { duration: '1s', target: 500 },
+    { duration: '10s', target: 1000 },
+    { duration: '10s', target: 2000 },
+    { duration: '1m', target: 2500 }
+  ]
 };
 
 export default function() {
   const listingId = Math.floor(Math.random() * (80000000)) + 1;
   http.get(`http://localhost:3000/api/homes/${listingId}`);
+  // http.get(`http://localhost:3000/api/homes/1`);
   sleep(1);
 }
